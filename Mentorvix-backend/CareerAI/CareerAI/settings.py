3a8ee4be19aef09ed6494af69aaca1a892cfd068
deepasree04 <deepasree1504@gmail.com>
2026-07-12 15:01:11 +0530

    fix: import os before using environment variables

--- Mentorvix-backend/CareerAI/CareerAI/settings.py
+++ Mentorvix-backend/CareerAI/CareerAI/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,10 +92,6 @@ CSRF_TRUSTED_ORIGINS = [
 
 ROOT_URLCONF = 'CareerAI.urls'
 
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 TEMPLATES = [
 {
@@ -154,7 +155,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-import os
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR.parent, '../Mentorvix-frontend')]
 
